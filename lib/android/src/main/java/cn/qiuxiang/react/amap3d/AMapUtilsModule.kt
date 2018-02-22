bec05240dbf3fe16b3bd28a232342ac2afb88c4f
zero <git@git.com>
2018-02-22 16:21:53 +0800

    增加坐标转换和获取设备坐标功能

--- lib/android/src/main/java/cn/qiuxiang/react/amap3d/AMapUtilsModule.kt
+++ lib/android/src/main/java/cn/qiuxiang/react/amap3d/AMapUtilsModule.kt
@@ -1,7 +1,12 @@
 package cn.qiuxiang.react.amap3d
 
 import android.content.res.Resources
+import com.amap.api.location.AMapLocation
+import com.amap.api.location.AMapLocationClient
+import com.amap.api.location.AMapLocationClientOption
+import com.amap.api.location.AMapLocationListener
 import com.amap.api.maps.AMapUtils
+import com.amap.api.maps.CoordinateConverter
 import com.amap.api.maps.model.LatLng
 import com.amap.api.services.core.AMapException
 import com.amap.api.services.help.Inputtips
@@ -10,9 +15,10 @@ import com.amap.api.services.help.Tip
 import com.facebook.react.bridge.*
 import com.facebook.react.modules.core.DeviceEventManagerModule
 import org.json.JSONArray
+import org.json.JSONObject
 
 @Suppress("unused")
-class AMapUtilsModule(private val reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext), Inputtips.InputtipsListener {
+class AMapUtilsModule(private val reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext), Inputtips.InputtipsListener, AMapLocationListener {
     override fun getName(): String {
         return "AMapUtils"
     }
@@ -50,6 +56,61 @@ class AMapUtilsModule(private val reactContext: ReactApplicationContext) : React
         } else {
             return
         }
+    }
+
+    @ReactMethod
+    fun getLocation() {
+        val locationClient = AMapLocationClient(reactContext)
+        val locationOption = AMapLocationClientOption()
+        //设置定位监听
+        locationClient.setLocationListener(this)
+        //设置定位模式为高精度模式，Battery_Saving为低功耗模式，Device_Sensors是仅设备模式
+        locationOption.locationMode = AMapLocationClientOption.AMapLocationMode.Hight_Accuracy
+        //设置定位间隔,单位毫秒,默认为2000ms
+        locationOption.interval = 2000
+        //设置定位参数
+        locationClient.setLocationOption(locationOption)
+        // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+        // 注意设置合适的定位时间的间隔（最小间隔支持为1000ms），并且在合适时间调用stopLocation()方法来取消定位请求
+        // 在定位结束后，在合适的生命周期调用onDestroy()方法
+        // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
+        // 启动定位
+        locationClient.startLocation()
+    }
+
+    override fun onLocationChanged(amapLocation: AMapLocation?) {
+        if (amapLocation != null && amapLocation.errorCode == 0) {
+            val latLng = LatLng(amapLocation.latitude, amapLocation.longitude)
+            val data = Arguments.createMap()
+            data.putDouble("latitude", latLng.latitude)
+            data.putDouble("longitude", latLng.longitude)
+            reactContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java).emit("onGetLocation", data)
+        } else {}
+    }
+
+    @ReactMethod
+    fun convertLatlng(latitude: Double, longitude: Double, coordType: String?, promise:Promise) {
+        val sourceLatLng = LatLng(latitude, longitude)
+        var coord = CoordinateConverter.CoordType.GPS
+        when(coordType){
+            "aliyun" -> coord = CoordinateConverter.CoordType.ALIYUN
+            "baidu" -> coord = CoordinateConverter.CoordType.BAIDU
+            "google" -> coord = CoordinateConverter.CoordType.GOOGLE
+            "mapabc" -> coord = CoordinateConverter.CoordType.MAPABC
+            "mapbar" -> coord = CoordinateConverter.CoordType.MAPBAR
+            "sosomap" -> coord = CoordinateConverter.CoordType.SOSOMAP
+        }
+        val converter = CoordinateConverter(reactContext)
+        // CoordType.GPS 待转换坐标类型
+        converter.from(coord)
+        // sourceLatLng待转换坐标点
+        converter.coord(sourceLatLng)
+        // 执行转换操作
+        val destLatLng = converter.convert()
+        var ret = JSONObject()
+        ret.put("latitude", destLatLng.latitude)
+        ret.put("longitude", destLatLng.longitude)
+        promise.resolve(ret.toString())
     }
 }
 
